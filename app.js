5d0a298d33bb1578c898c8a95d0201c076dd1d2b
Hein Htet Aung <hx.htetaung@gmail.com>
2022-07-16 17:29:51 +0630

    modal error hasnt been solved

---
+++ app.js
@@ -0,0 +1,12 @@
+// const btnClose = document.querySelector(".close-btn");
+// const modal = document.querySelector(".modal");
+
+// const btnExit = document.querySelector(".exit-btn");
+
+// btnClose.addEventListener("click", () => {
+//   modal.showModal();
+// });
+
+// btnExit.addEventListener("click", () => {
+//   modal.close();
+// });
